6ca999f1ed3749660b1d8c67e9432fb65a0943c2
ZARONIKOS Kostas <ZaronikosK@unisystems.gr>
2025-05-13 20:53:43 +0300

    feat: add hello world

---
+++ src/gr/aub/cf/helloapp/HelloWorld.java
@@ -0,0 +1,10 @@
+package gr.aub.cf.helloapp;
+
+/**
+ * This project created to show how
+ * to make a git project and then how
+ * to stage and commit changes in local repo.
+ * And then how to push to GitHub.
+ */
+public class HelloWorld {
+}
